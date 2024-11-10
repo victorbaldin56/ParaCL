9d9fa1f0a58470c8b5a9031f20e29f19574360d4
Victor Baldin <victor.baldin.2005@gmail.com>
2024-11-10 16:21:36 +0300

    restruct & add ast prototype

---
+++ ast/inode.hh
@@ -0,0 +1,13 @@
+#ifndef PARACL_AST_INODE_HH_
+#define PARACL_AST_INODE_HH_
+
+namespace ast {
+
+class INode {
+ public:
+  virtual ~INode() {}
+};
+
+}
+
+#endif // PARACL_AST_INODE_HH_
---
+++ ast/node.cc
@@ -0,0 +1,7 @@
+#include "node.hh"
+
+namespace ast {
+
+
+
+}
