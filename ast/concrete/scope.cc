f71aa60c478fc95d44e779a50726822e429427c7
Victor Baldin <victor.baldin.2005@gmail.com>
2024-11-14 13:35:55 +0300

    write Scope::calc()

--- ast/concrete/scope.cc
+++ ast/concrete/scope.cc
@@ -6,4 +6,11 @@ pIScope makeScope(const pIScope& par) {
   return std::make_shared<Scope>(par);
 }
 
+IntT Scope::calc() const {
+  for (const pINode p : stms_) {
+    p->calc();
+  }
+  return 0; // FIXME
+}
+
 } // namespace ast
