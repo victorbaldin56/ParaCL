6dee12748d8fda11a03ef55b90367913234e82f0
Victor Baldin <victor.baldin.2005@gmail.com>
2024-12-04 19:42:44 +0300

    add invalid tests

--- parser/lexer.cc
+++ parser/lexer.cc
@@ -9,7 +9,7 @@ void PLexer::updateCurrentLocation() noexcept {
   cur_loc_.begin.line = cur_loc_.end.line = cur_line;
 
   if (isEmptyLine(yytext)) {
-    cur_loc_.begin.column = cur_loc_.end.column = yyleng;
+    cur_loc_.begin.column = cur_loc_.end.column = 1;
   } else {
     cur_loc_.begin.column = prev_end_col;
     cur_loc_.end.column = cur_loc_.begin.column + yyleng;
