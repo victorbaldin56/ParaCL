bba5a46953fa77eb7a520a609274c210152a4a2b
Victor Baldin <victor.baldin.2005@gmail.com>
2025-02-06 19:11:13 +0300

    draft

---
+++ ast/include/ast/codegen.hh
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <map>
+#include <memory>
+
+#include "llvm/ADT/APInt.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Value.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Scalar/GVN.h"
+#include "llvm/Transforms/Utils.h"
+
+class CodeGen final {
+  llvm::LLVMContext context_;
+  llvm::IRBuilder<> builder_;
+  std::unique_ptr<llvm::Module> module_;
+  std::map<std::string, std::shared_ptr<llvm::Value>> named_values_;
+};
+
+extern std::unique_ptr<CodeGen> global_gen;
