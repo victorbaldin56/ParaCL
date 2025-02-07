d101b8004c153242f5ba409db2c9799985f67f07
Victor Baldin <victor.baldin.2005@gmail.com>
2025-02-08 01:56:00 +0300

    add incr_decr

--- ast/include/ast/var_node.hh
+++ ast/include/ast/var_node.hh
@@ -15,6 +15,20 @@ class VarNode : public INode {
   int calc() const override { return it_->second.value; }
 
   void assign(int val) { it_->second.value = val; }
+  int preIncrement() { return ++it_->second.value; }
+  int preDecrement() { return --it_->second.value; }
+
+  int postIncrement() {
+    int tmp = it_->second.value;
+    ++it_->second.value;
+    return tmp;
+  }
+
+  int postDecrement() {
+    int tmp = it_->second.value;
+    --it_->second.value;
+    return tmp;
+  }
 };
 
 } // namespace ast
